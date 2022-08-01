e78051c6b09b4ce14cdd3270c257911e41f86584
Amandeep Kaur <amandeep.kaur@fullstacklabs.ca>
2022-08-01 11:14:29 -0400

    first commit

---
+++ src/main/java/com/example/jenkin_demo1/JenkinDemo1Application.java
@@ -0,0 +1,14 @@
+package com.example.jenkin_demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinDemo1Application.class, args);
+		System.out.println("heloo jenkin!!!!!!");
+	}
+
+}
