510340ecba91b233602789fa1eaa335328f2ec9b
Amandeep Kaur <amandeep.kaur@fullstacklabs.ca>
2022-08-03 09:55:34 -0400

    commit

--- src/main/java/com/example/jenkin_demo1/WebController.java
+++ src/main/java/com/example/jenkin_demo1/WebController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WebController {
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String get()
     {
         return "Welcome In This Api";
