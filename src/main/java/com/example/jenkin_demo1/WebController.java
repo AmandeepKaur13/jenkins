169e470dff233a6a51a0b145c39b98fd60f4ea97
Amandeep Kaur <amandeep.kaur@fullstacklabs.ca>
2022-08-04 14:25:50 -0400

    commit

--- src/main/java/com/example/jenkin_demo1/WebController.java
+++ src/main/java/com/example/jenkin_demo1/WebController.java
@@ -1,9 +1,11 @@
 package com.example.jenkin_demo1;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/jenkins")
 public class WebController {
     @GetMapping("/hello")
     public String get()
