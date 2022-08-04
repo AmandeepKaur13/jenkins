40a34f5729ae0a690f5312719fd39694ddd4be82
Amandeep Kaur <amandeep.kaur@fullstacklabs.ca>
2022-08-04 16:02:11 -0400

    commit

--- src/main/java/com/example/jenkin_demo1/JenkinDemo1Application.java
+++ src/main/java/com/example/jenkin_demo1/JenkinDemo1Application.java
@@ -2,9 +2,11 @@ package com.example.jenkin_demo1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class JenkinDemo1Application {
 
 	public static void main(String[] args) {
